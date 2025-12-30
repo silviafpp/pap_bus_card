214d0f74f6815b4fad2007106135adfdd3441f17
Silvia Pacheco <silviapacheco724@gmail.com>
2025-12-30 22:31:46 +0000

    first files

---
+++ backend/connect_db.py
@@ -0,0 +1,11 @@
+import pg8000
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PORT = int(os.getenv("DB_PORT"))
